afa7c9ebff85421b03510f6227dae6740c320a34
shell284u <103103278+shell284u@users.noreply.github.com>
2022-04-19 18:40:24 +0530

    Add files via upload

---
+++ Mycaption_project.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pi= 3.141
+r = float(input("input radius of the circle:"))
+a= pi*r*r
+print("area of radius is %.2f"%a)
+
+
+# In[6]:
+
+
+fn= input("Output of the file: ")
+
+f = fn.split(".")
+
+print ("The extension of the file is : " + f[-1])
+
+
+# In[2]:
+
+
+def __init__(self, x, y, z): 
+       self.a = x + y + z 
+       x = change(1,2,3)
+       y = getattr(x, 'a')
+       setattr(x, 'a', y+1) 
+       print(x.a)
+
+
+# In[ ]:
+
+
+
+
