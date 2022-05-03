baeda0b8b518076f763d024a505fb038ac22affc
shell284u <103103278+shell284u@users.noreply.github.com>
2022-05-03 14:50:14 +0530

    Add files via upload

---
+++ funtions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print (most_frequent('Mississippi'))
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
