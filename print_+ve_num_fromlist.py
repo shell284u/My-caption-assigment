810657c8fe190faaa1e8a1f21ebe88780773828f
shell284u <103103278+shell284u@users.noreply.github.com>
2022-05-03 14:29:42 +0530

    Add files via upload

---
+++ print_+ve_num_fromlist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+
+list1 = [11, 1,-9, 45, 66]
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
